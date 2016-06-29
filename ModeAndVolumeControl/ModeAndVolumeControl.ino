272f0044902c213c60423fb18fb0ad77e6e10efb
rumackr <rumackr@msoe.edu>
2016-06-28 22:30:06 -0500

    initial commit

--- ModeAndVolumeControl/ModeAndVolumeControl.ino
+++ ModeAndVolumeControl/ModeAndVolumeControl.ino
@@ -1,11 +1,10 @@
-//V1
 /**********************************************************************
 * FILENAME:  ModeAndVolumeControl.ino
 * AUTHOR:    reidrumack@gmail.com <Reid Rumack>
-* DATE:      23 June 2016
+* DATE:      28 June 2016
 * Arduino:   Mini Pro
 * PRODUCT:   LCD 1602A, 5 way nav switch and Game Switch (lever switch)
-* VERSION:   1.1
+* VERSION:   2.0
 **********************************************************************/
 #include <SoftwareSerial.h>
 #include <LiquidCrystal.h>
@@ -14,13 +13,12 @@
  * PIN Defineintions  
  */
 
-
-
 // difines the pin that the switch are are on
 
 
 // amount +/- ADC value alowed for error
 #define ERR 30
+#define IRQ 2
 
 #define SWITCHS A0
 
@@ -43,20 +41,19 @@ LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
 SoftwareSerial ss = SoftwareSerial(SFX_TX, SFX_RX);
 Adafruit_Soundboard sfx = Adafruit_Soundboard(&ss, NULL, SFX_RST);
 
+
+
 volatile uint8_t CURRENTMODE = 0;
 volatile uint8_t LCDMODE = 0;
 volatile uint8_t VOLUME = 15;
+volatile  unsigned long last_interrupt_time = 0;
 
 static  char* modeString[]= {"   Semi  Auto", "   Full  Auto", "  Light It Up", "    Lean On"};
 
-
 void setup(){
     pinMode(2, INPUT);
-    Serial.begin(9600);
-    Serial.print("pwr");
     ss.begin(9600);
     lcd.begin(16, 2);
-    while(!Serial);
     lcd.clear();
     for(uint8_t i = 0; i < VOLUME;i++){
         lcd.setCursor(i,1);
@@ -64,42 +61,51 @@ void setup(){
     }
     lcd.setCursor(0,0);
     lcd.print(modeString[CURRENTMODE]);
+    interrupts(); 
+    attachInterrupt(digitalPinToInterrupt(2), isr, HIGH); 
+    wait();  
 }
 
 void loop(){
-    uint16_t adcValue = analogRead(SWITCHS);
-    if(adcValue >= 30 ){
-        btnHandler(adcValue);
-    }
 }
 
-void btnHandler(uint16_t adcValue){
-    if(adcValue > TRIGGER_ADC - 10 ){
-        trigger();
-    }else if ((adcValue >= UP_ADC - ERR) && (adcValue <= UP_ADC + ERR)){
-        volumeUp(); 
-    }else if ((adcValue >= DOWN_ADC - ERR) && (adcValue <= DOWN_ADC + ERR)){
-        volumeDown();
-    }else if ((adcValue >= LEFT_ADC - ERR) && (adcValue <= LEFT_ADC + ERR)){
-        cycleLeft();
-    }else if ((adcValue >= RIGHT_ADC - ERR) && (adcValue <= RIGHT_ADC + ERR)){
-        cycleRight();
-    }else if ((adcValue >= CENTER_ADC - ERR) && (adcValue <= CENTER_ADC + ERR)){
-        selectMode();
+void isr(){
+    uint16_t adcValue = analogRead(SWITCHS);
+    noInterrupts(); 
+    unsigned long interrupt_time = millis();
+ // If interrupts come faster than 200ms, assume it's a bounce and ignore
+    if (interrupt_time - last_interrupt_time > 200){
+        if(adcValue > TRIGGER_ADC - 10 ){
+            trigger();
+        }else if ((adcValue >= UP_ADC - ERR) && (adcValue <= UP_ADC + ERR)){
+            volumeUp(); 
+        }else if ((adcValue >= DOWN_ADC - ERR) && (adcValue <= DOWN_ADC + ERR)){
+            volumeDown();
+        }else if ((adcValue >= LEFT_ADC - ERR) && (adcValue <= LEFT_ADC + ERR)){
+            cycleLeft();
+        }else if ((adcValue >= RIGHT_ADC - ERR) && (adcValue <= RIGHT_ADC + ERR)){
+            cycleRight();
+        }else if ((adcValue >= CENTER_ADC - ERR) && (adcValue <= CENTER_ADC + ERR)){
+            selectMode();
+        } 
     }
+      last_interrupt_time = interrupt_time;
+      interrupts();  
 }
+    
 
 void trigger(){
     uint8_t toPlay = 0;
-    sfx.rest();
-    sfx.playTrack(toPlay);
-    if(LCDMODE != CURRENTMODE){
-        LCDMODE = CURRENTMODE;
-        clearLcdTopLine();   
-        lcd.setCursor(0,0);
-        lcd.print(modeString[CURRENTMODE]);
-    }
-    delay(250);
+    //sfx.reset();
+    //sfx.playTrack(toPlay);
+     lcd.print("Fire!");
+
+//    if(LCDMODE != CURRENTMODE){
+//        LCDMODE = CURRENTMODE;
+//        clearLcdTopLine();   
+//        lcd.setCursor(0,0);
+//        lcd.print(modeString[CURRENTMODE]);
+//    }
 }
 
 void volumeUp(){
@@ -111,7 +117,6 @@ void volumeUp(){
     }
     lcd.setCursor(VOLUME - 1,1);
     lcd.write(0xFF);
-    delay(500);
 }
 
 void volumeDown(){
@@ -123,7 +128,6 @@ void volumeDown(){
     }
     lcd.setCursor(VOLUME,1);
     lcd.write(' ');
-    delay(500);
 }
 
 void cycleRight(){
@@ -131,7 +135,6 @@ void cycleRight(){
     clearLcdTopLine();
     lcd.setCursor(0,0);
     lcd.print(modeString[LCDMODE]);
-    delay(500);
 }
 
 void cycleLeft(){
@@ -139,7 +142,6 @@ void cycleLeft(){
     clearLcdTopLine();
     lcd.setCursor(0,0);
     lcd.print(modeString[LCDMODE]);
-    delay(500);
 }
 
 void selectMode(){
